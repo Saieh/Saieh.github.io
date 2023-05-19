e47363338abcf2a1a5b346f133273fef3b2abbb2
Saieh <45912557+Saieh@users.noreply.github.com>
2023-05-19 12:49:11 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var a = document.querySelector("canvas");
-a.addEventListener("click", f);
-function f() {
- if (document.getElementById("aud").paused) document.getElementById("aud").play();
- else document.getElementById("aud").pause();
-}
